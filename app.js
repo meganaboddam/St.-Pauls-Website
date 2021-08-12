e631d0fdc2b04641408714b187913adccb11c1e8
meganaboddam <meganaboddam@gmail.com>
2021-08-11 19:11:22 -0700

    started home page

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
+/* mobile nav bar creation */
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('nav_bar__menu');
 
+// displays mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+
+
+
+/*
+const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('alternate-font')
     
@@ -15,3 +30,4 @@ switcher.addEventListener('click', function() {
 
 }
 );
+*/
